f4be33ed97d7d5e455c64e48764b9d151f76c8c8
Adrian Murillo <amurill9@ucsc.edu>
2019-12-19 20:59:11 -0600

    Added Script

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentHealth--;
+        }
+    }
+}
